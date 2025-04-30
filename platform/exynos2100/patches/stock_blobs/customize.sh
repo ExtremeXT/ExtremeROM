bab9e937382c93a31aa2ea50d232f5e3ba20b363
nv2500 <1527411+nv2500@users.noreply.github.com>
2025-04-29 23:32:18 -0300

    Update customize.sh

--- platform/exynos2100/patches/stock_blobs/customize.sh
+++ platform/exynos2100/patches/stock_blobs/customize.sh
@@ -2,7 +2,7 @@
 # S21 Series -> SoundBooster 1050
 echo "Replacing SoundBooster"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2060.so"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1070.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 echo "Replacing GameDriver"
