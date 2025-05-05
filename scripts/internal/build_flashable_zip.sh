5d8089b9c3da503f9db736fbc6b3e64079de2807
nv2500 <1527411+nv2500@users.noreply.github.com>
2025-05-05 10:31:39 -0300

    Update build_flashable_zip.sh - keep xxx.img built in `working` folder for future use

--- scripts/internal/build_flashable_zip.sh
+++ scripts/internal/build_flashable_zip.sh
@@ -551,7 +551,7 @@ while read -r i; do
     fi
     bash "$SRC_DIR/scripts/build_fs_image.sh" "$FILESYSTEM_TYPE+sparse" "$WORK_DIR/$PARTITION" \
         "$WORK_DIR/configs/file_context-$PARTITION" "$WORK_DIR/configs/fs_config-$PARTITION" > /dev/null 2>&1
-    mv "$WORK_DIR/$PARTITION.img" "$TMP_DIR/$PARTITION.img"
+    cp "$WORK_DIR/$PARTITION.img" "$TMP_DIR/$PARTITION.img"
     echo "Building $PARTITION.img - DONE"
 done <<< "$(find "$WORK_DIR" -mindepth 1 -maxdepth 1 -type d)"
 
