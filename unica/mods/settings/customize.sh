6b71e133f14904e82da4f71da2dfc362d8dcbc21
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-05-31 14:50:03 +0300

    unica: mods: settings: disable ASKS
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -10,3 +10,7 @@
 # for f in $FTP; do
 #     sed -i "s/SM-A236B/SM-S938B/g" "$APKTOOL_DIR/$f"
 # done
+
+# ro.build.2ndbrand is always "false"
+echo "Disabling ASKS"
+sed -i "s/ro.build.official.release/ro.build.2ndbrand/g" "$APKTOOL_DIR/system/framework/services.jar/smali/com/android/server/asks/ASKSManagerService.smali"
