89c352403cde191a4f8f412a0293a725673be444
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-09-21 11:59:51 +0300

    target: r8s: bt-lib-patch: fixup
    Change-Id: Ief7971a0f10d5b1171e61960018ea5dce33cd53f
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- target/r8s/patches/bt-lib-patch/customize.sh
+++ target/r8s/patches/bt-lib-patch/customize.sh
@@ -1,10 +1,10 @@
 if [ ! -f "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" ]; then
-    LOG_STEP_IN "- Extracting libbluetooth_jni.so from com.android.btservices.apex"
+    LOG_STEP_IN "- Extracting libbluetooth_jni.so from com.android.bt.apex"
 
     [ -d "$TMP_DIR" ] && EVAL "rm -rf \"$TMP_DIR\""
     mkdir -p "$TMP_DIR"
 
-    EVAL "unzip -j \"$WORK_DIR/system/system/apex/com.android.btservices.apex\" \"apex_payload.img\" -d \"$TMP_DIR\""
+    EVAL "unzip -j \"$WORK_DIR/system/system/apex/com.android.bt.apex\" \"apex_payload.img\" -d \"$TMP_DIR\""
 
     if ! sudo -n -v &> /dev/null; then
         LOG "\033[0;33m! Asking user for sudo password\033[0m"
@@ -27,4 +27,4 @@ fi
 
 # https://github.com/duhansysl/Bluetooth-Library-Patcher/blob/67e598ad142ed296b487a7a4585927c993d4f35d/hexpatcher.sh#L43
 HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-    "1ff828ab5e39480500352800805228ab" "1ff828ab5e392a0000142800805228ab"
+    "289777394805003736008052" "289777392a00001436008052"
